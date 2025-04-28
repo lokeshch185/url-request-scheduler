baa9a73d1388f4859d918dc48947b5d3e5a23cf8
lokesh chaudhari <47492669+lokeshch185@users.noreply.github.com>
2025-04-28 18:28:40 +0530

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const app = express();
 const PORT = 5000;
 
 // URL to send GET request to
-const targetUrls = ['https://real-time-chat-app-0030.onrender.com/','https://sarthi-dmpe.onrender.com/'];
+const targetUrls = ['your urls here'];
 
 // Function to send GET request
 const sendGetRequest = async () => {
